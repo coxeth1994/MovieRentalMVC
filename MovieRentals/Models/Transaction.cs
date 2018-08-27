0904a2668da93777e94d1ef390fa663b8e471292
ec513234 <coxeth1994@gmail.com>
2018-08-27 18:34:29 -0500

    add transaction to models

---
+++ MovieRentals/Models/Transaction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieRentals.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public Movie[] Movies { get; set; }
+        public decimal Total { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public Transaction() { }
+
+        public Transaction(int id, Customer customer, Movie[] movies, decimal total, DateTime orderDate)
+        {
+            Id = id;
+            Customer = customer;
+            Movies = movies;
+            Total = total;
+            OrderDate = orderDate;
+        }
+    }
+}
