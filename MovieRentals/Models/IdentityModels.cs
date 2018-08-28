099c0f324e6c1255de07897a219e54cd001e2a76
mylesmolson <myles.olson@southeast.edu>
2018-08-27 19:42:11 -0500

    Reworded home page and links. Fixed layout of Movie views.

--- MovieRentals/Models/IdentityModels.cs
+++ MovieRentals/Models/IdentityModels.cs
@@ -34,7 +34,5 @@ namespace MovieRentals.Models
         {
             return new ApplicationDbContext();
         }
-
-        public System.Data.Entity.DbSet<MovieRentals.Models.Transaction> Transactions { get; set; }
     }
 }
