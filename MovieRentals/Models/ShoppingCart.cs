9882ca115ea0c107e9a8e2a09f1c26a05a8320f2
mylesmolson <myles.olson@southeast.edu>
2018-08-29 19:47:20 -0500

    Created ShoppingCart model and empty controller.

---
+++ MovieRentals/Models/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieRentals.Models
+{
+    public class ShoppingCart
+    {
+        public Movie[] Movies { get; set; }
+        public decimal Total { get; set; }
+
+        public ShoppingCart() { }
+        public ShoppingCart(Movie[] theMovies, decimal theTotal)
+        {
+            Movies = theMovies;
+            Total = theTotal;
+        }
+    }
+}
